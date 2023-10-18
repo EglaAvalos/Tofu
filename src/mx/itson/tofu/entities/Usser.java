3c0497c19e8f8b354be6673dfdd8270e21990af6
avalo <avalo@Icely>
2023-10-17 23:01:00 -0700

    Se agrego una entidad usuario

---
+++ src/mx/itson/tofu/entities/Usser.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.tofu.entities;
+
+/**
+ *
+ * @author avalo
+ */
+public class Usser {
+    String name;
+    String email;
+    String password;
+}
