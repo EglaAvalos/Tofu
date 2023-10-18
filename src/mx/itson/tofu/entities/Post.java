e546d66c5931c1dd59eb32131479f0788112d6df
avalo <avalo@Icely>
2023-10-17 23:11:01 -0700

    Se agrego una entidad post sus atributos y el encapsulamiento

--- src/mx/itson/tofu/entities/Post.java
+++ src/mx/itson/tofu/entities/Post.java
@@ -10,11 +10,111 @@ package mx.itson.tofu.entities;
  * @author avalo
  */
 public class Post {
-    String description;
-    Usser user;
-    int like;
-    int dislike;
-    String comments;
+    private String description;
+    private Usser user;
+    private int like;
+    private int dislike;
+    private String comments;
+    private String date;
+    private Category category;
+
+    /**
+     * @return the description
+     */
+    public String getDescription() {
+        return description;
+    }
+
+    /**
+     * @param description the description to set
+     */
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    /**
+     * @return the user
+     */
+    public Usser getUser() {
+        return user;
+    }
+
+    /**
+     * @param user the user to set
+     */
+    public void setUser(Usser user) {
+        this.user = user;
+    }
+
+    /**
+     * @return the like
+     */
+    public int getLike() {
+        return like;
+    }
+
+    /**
+     * @param like the like to set
+     */
+    public void setLike(int like) {
+        this.like = like;
+    }
+
+    /**
+     * @return the dislike
+     */
+    public int getDislike() {
+        return dislike;
+    }
+
+    /**
+     * @param dislike the dislike to set
+     */
+    public void setDislike(int dislike) {
+        this.dislike = dislike;
+    }
+
+    /**
+     * @return the comments
+     */
+    public String getComments() {
+        return comments;
+    }
+
+    /**
+     * @param comments the comments to set
+     */
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    /**
+     * @return the date
+     */
+    public String getDate() {
+        return date;
+    }
+
+    /**
+     * @param date the date to set
+     */
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    /**
+     * @return the category
+     */
+    public Category getCategory() {
+        return category;
+    }
+
+    /**
+     * @param category the category to set
+     */
+    public void setCategory(Category category) {
+        this.category = category;
+    }
     
     
 }
