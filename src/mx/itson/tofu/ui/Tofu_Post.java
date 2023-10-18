0a3a0e03ff09f26ef8ab85e4306189fa9b255cd9
avalo <avalo@Icely>
2023-10-18 09:40:34 -0700

    Se agregan datos para imprimir

--- src/mx/itson/tofu/ui/Tofu_Post.java
+++ src/mx/itson/tofu/ui/Tofu_Post.java
@@ -22,7 +22,7 @@ public class Tofu_Post {
         user.setPassword("kuromi23");
         
         Usser user2 = new Usser ();
-        user2.setName("Marian Moreno");
+        user2.setName("Mariana Moreno");
         user2.setEmail("yoonie_fresita23@gmail.com");
         user2.setPassword("minyO0ngi");
         
@@ -31,12 +31,20 @@ public class Tofu_Post {
         
         Post post = new Post ();
         post.setTitle("La adopción en la nube");
-        post.setDescription("La computación en la nube ha transformado la forma en que se desarrolla y se despliega el software. "
-                + "\nPermite a los desarrolladores acceder a recursos escalables y flexibles, lo que facilita la creación y la"
-                + "\ngestión de aplicaciones. La nube también ha permitido la aparición de servicios de software como servicio (SaaS),"
-                + "\ndonde los usuarios pueden acceder a aplicaciones directamente a través de Internet sin necesidad de instalarlas en sus dispositivos.");
+        post.setDescription("La computación en la nube ha transformado la forma en que se desarrolla y se despliega el software \nPermite a los desarrolladores acceder a recursos escalables y flexibles, lo que facilita la creación y la\ngestión de aplicaciones. La nube también ha permitido la aparición de servicios de software como servicio (SaaS), \ndonde los usuarios pueden acceder a aplicaciones directamente a través de Internet sin necesidad de instalarlas en sus dispositivos.");
+        post.setComments(user2.getName()+": Orale que padre");
+        post.setLike(23);
+        post.setDislike(10);
+        post.setDate("18 de octubre del 2023");
+        post.setUser(user);
+        post.setUser(user2);
         post.getCategory();
-        System.out.println(post.getDescription());
+        
+        System.out.println("-------------"+post.getTitle().toUpperCase()+"-------------");
+        System.out.println("Descripción: " + post.getDescription());
+        System.out.println("By: " + post.getUser().getName());
+        System.out.println("LIKES " + post.getLike() + "      DISLIKES " + post.getDislike());
+        
        
     }
     
