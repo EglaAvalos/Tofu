ee80dff277bfef943a1a6f897fb2e33050bc7f54
avalo <avalo@Icely>
2023-10-17 23:06:48 -0700

    Se agrego una entidad post

---
+++ src/mx/itson/tofu/entities/Post.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mx.itson.tofu.entities;
+
+/**
+ *
+ * @author avalo
+ */
+public class Post {
+    String description;
+    Usser user;
+    int like;
+    int dislike;
+    
+}
